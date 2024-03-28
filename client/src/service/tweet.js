4c421e707334d2936b0b38cf8a2d8969b7850814
Jaewoong <dixk3458@naver.com>
2024-03-28 18:45:23 +0900

    프론트엔드 구현

--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,43 +1,69 @@
 export default class TweetService {
-  tweets = [
-    {
-      id: 1,
-      text: '드림코딩에서 강의 들으면 너무 좋으다',
-      createdAt: '2021-05-09T04:20:57.000Z',
-      name: 'Bob',
-      username: 'bob',
-      url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-    },
-  ];
+  constructor(baseURL) {
+    this.baseURL = baseURL;
+  }
 
   async getTweets(username) {
-    return username
-      ? this.tweets.filter((tweet) => tweet.username === username)
-      : this.tweets;
+    // username 쿼리에 따라 달라진다.
+    const query = username ? `?username=${username}` : '';
+
+    const response = await fetch(`${this.baseURL}/posts${query}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      throw new Error(data.message);
+    }
+
+    return data;
   }
 
   async postTweet(text) {
-    const tweet = {
-      id: Date.now(),
-      createdAt: new Date(),
-      name: 'Ellie',
-      username: 'ellie',
-      text,
-    };
-    this.tweets.push(tweet);
-    return tweet;
-  }
+    const response = await fetch(`${this.baseURL}/posts`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        text: text,
+        name: 'Ellie',
+        username: 'ellie',
+      }),
+    });
 
-  async deleteTweet(tweetId) {
-    this.tweets = this.tweets.filter((tweet) => tweet.id !== tweetId);
-  }
+    const data = await response.json();
 
-  async updateTweet(tweetId, text) {
-    const tweet = this.tweets.find((tweet) => tweet.id === tweetId);
-    if (!tweet) {
-      throw new Error('tweet not found!');
+    if (response.status !== 201) {
+      throw new Error(data.message);
     }
-    tweet.text = text;
-    return tweet;
+    return data;
+  }
+
+  async deleteTweet(postId) {
+    const response = await fetch(`${this.baseURL}/posts/${postId}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.status !== 204) {
+      throw new Error();
+    }
+  }
+
+  async updateTweet(postId, text) {
+    const response = await fetch(`${this.baseURL}/posts/${postId}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ text: text }),
+    });
+
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      throw new Error(data.message);
+    }
+
+    return data;
   }
 }
