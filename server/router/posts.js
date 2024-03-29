4560145cf3f04514c01ba31541f0c923429c8a24
Jaewoong <dixk3458@naver.com>
2024-03-29 16:44:13 +0900

    validation 검증

--- server/router/posts.js
+++ server/router/posts.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import { body } from 'express-validator';
 import * as postsController from '../controller/posts.js';
+import validate from '../middleware/validator.js';
 
 // GET /posts
 // GET /posts?username=username
@@ -10,13 +12,21 @@ import * as postsController from '../controller/posts.js';
 
 const router = express.Router();
 
+const validatePost = [
+  body('text')
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage('text shoud be at least 5 characters'),
+  validate,
+];
+
 router.get('/', postsController.getPosts);
 
 router.get('/:id', postsController.getPost);
 
-router.post('/', postsController.createPost);
+router.post('/', validatePost, postsController.createPost);
 
-router.put('/:id', postsController.updatePost);
+router.put('/:id', validatePost, postsController.updatePost);
 
 router.delete('/:id', postsController.removePost);
 
